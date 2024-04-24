af4012dd5d57dba8c53e2b555ae02de894f445dd
wikto <wikto@DESKTOP-NDAD44Q>
2024-04-24 19:05:13 +0200

    Dodaj pliki projektów.

---
+++ Algorytmy Szukające/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LinearSearchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            int[] array = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+
+            int searchValue = 50;
+
+            int linearSearchResult = LinearSearch(array, searchValue);
+            Console.WriteLine($"Wynik wyszukiwania liniowego: {linearSearchResult}");
+
+            int sentinelSearchResult = SentinelLinearSearch(array, searchValue);
+            Console.WriteLine($"Wynik wyszukiwania liniowego z wartownikiem: {sentinelSearchResult}");
+        }
+
+
+        static int LinearSearch(int[] array, int searchValue)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == searchValue)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static int SentinelLinearSearch(int[] array, int searchValue)
+        {
+   
+            array[array.Length - 1] = searchValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == searchValue)
+                {
+                   
+                    if (i == array.Length - 1)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return i; 
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
